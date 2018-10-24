250e5eb93004a5c65f7a02f22a06d3f36a2fa120
Shailesh <mshaileshr@gmail.com>
2018-10-24 19:01:34 +0530

    removed api_key, delivery_token, environment from repository

--- app/src/main/java/sync/contentstack/playground/BaseApp.java
+++ app/src/main/java/sync/contentstack/playground/BaseApp.java
@@ -1,6 +1,5 @@
 package sync.contentstack.playground;
 
-import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -8,9 +7,9 @@ import android.util.Log;
 import com.contentstack.sdk.Config;
 import com.contentstack.sdk.Contentstack;
 import com.contentstack.sdk.Stack;
-
 import static sync.contentstack.playground.BuildConfig.PLAYGROUND_SYNC_TOKEN;
 import static sync.contentstack.playground.BuildConfig.SYNC_TOKEN_KEY;
+
 
 public class BaseApp extends Application {
 
@@ -20,16 +19,13 @@ public class BaseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         SHARED_PREFERENCES = getSharedPreferences(PLAYGROUND_SYNC_TOKEN, Context.MODE_PRIVATE);
 
         try {
 
             Config config = new Config();
             config.setHost(BuildConfig.STACK_URL);
-            stackInstance = Contentstack.stack(getApplicationContext(), BuildConfig.API_KEY,
-                    BuildConfig.DELIVERY_TOKEN, BuildConfig.ENVIRONMENT, config);
-
+            stackInstance = Contentstack.stack(getApplicationContext(), BuildConfig.API_KEY, BuildConfig.DELIVERY_TOKEN, BuildConfig.ENVIRONMENT, config);
         } catch (Exception e) {
             Log.e("Base App", "Exception occurred");
             e.printStackTrace();
@@ -42,13 +38,10 @@ public class BaseApp extends Application {
     }
 
 
-    public static String getSyncToken() {
-        return SHARED_PREFERENCES.getString(SYNC_TOKEN_KEY, null);
-    }
+    public static String getSyncToken() { return SHARED_PREFERENCES.getString(SYNC_TOKEN_KEY, null); }
 
 
     public static void storeSyncToken(String sync_token) {
-
         SharedPreferences.Editor editor = SHARED_PREFERENCES.edit();
         editor.putString(SYNC_TOKEN_KEY, sync_token);
         editor.apply();
