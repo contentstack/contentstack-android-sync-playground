93d7d2409919004c1ebaf04084c3e65473e81ff2
Shailesh <mshaileshr@gmail.com>
2018-10-25 12:30:16 +0530

    removed api_key, delivery_token, environment from repository

--- app/src/main/java/sync/contentstack/playground/BaseApp.java
+++ app/src/main/java/sync/contentstack/playground/BaseApp.java
@@ -7,47 +7,73 @@ import android.util.Log;
 import com.contentstack.sdk.Config;
 import com.contentstack.sdk.Contentstack;
 import com.contentstack.sdk.Stack;
+
+import javax.security.auth.login.LoginException;
+
 import static sync.contentstack.playground.BuildConfig.PLAYGROUND_SYNC_TOKEN;
 import static sync.contentstack.playground.BuildConfig.SYNC_TOKEN_KEY;
 
 
 public class BaseApp extends Application {
 
+    //Stack instance variable
     private static Stack stackInstance;
+
+    // shared pref for saving pagination token/ sync token
     private static SharedPreferences SHARED_PREFERENCES;
+
+    // String shared pref KEY
+    private final String SHARED_PREF = BaseApp.class.getPackage().getName();
 
     @Override
     public void onCreate() {
         super.onCreate();
-        SHARED_PREFERENCES = getSharedPreferences(PLAYGROUND_SYNC_TOKEN, Context.MODE_PRIVATE);
+
+        /*Initialise shared preference*/
+        SHARED_PREFERENCES = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
 
         try {
 
+            // stack config to set host url
             Config config = new Config();
+            // set your host url
             config.setHost(BuildConfig.STACK_URL);
+            // To initialise Stack provide api_key, delivery_token & environment
             stackInstance = Contentstack.stack(getApplicationContext(), BuildConfig.API_KEY, BuildConfig.DELIVERY_TOKEN, BuildConfig.ENVIRONMENT, config);
+
         } catch (Exception e) {
+            // In case app meets Exception
             Log.e("Base App", "Exception occurred");
             e.printStackTrace();
         }
     }
 
 
+    /**
+     *
+     * @return stack instance
+     */
     public static Stack getStack(){
         return stackInstance;
     }
 
 
+    /**
+     *
+     * @return @{@link String} sync_token
+     */
     public static String getSyncToken() { return SHARED_PREFERENCES.getString(SYNC_TOKEN_KEY, null); }
 
 
+    /**
+     *
+     * @param sync_token accepted sync token @{@link String} to persists
+     */
     public static void storeSyncToken(String sync_token) {
         SharedPreferences.Editor editor = SHARED_PREFERENCES.edit();
         editor.putString(SYNC_TOKEN_KEY, sync_token);
         editor.apply();
     }
-
-
 
 
 }
